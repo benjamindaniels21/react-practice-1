1611cd7bbdc7547e697c80035a840e8e4b3ff1f4
Ben Daniels <benjamindaniels21@gmail.com>
2022-09-17 19:34:28 -0400

    passing props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import List from "./List";
+import Message from "./Message";
 
 function App() {
   return (
     <div className="App">
       <List />
+      <Message message="This is a message passed in via props" />
     </div>
   );
 }
